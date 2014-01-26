b02c432a121b84c009ebd1a03081d13730955c9a
mits <mits.jp@gmail.com>
2014-01-26 13:11:35 +0900

    演習１６作成

--- IS-prac16/AppDelegate.m
+++ IS-prac16/AppDelegate.m
@@ -26,6 +26,10 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    NSLog(@"アプリがバックグランドになります");
+    [[[UIAlertView alloc] initWithTitle:@"appBackground" message:@"アプリがバックグランドになります" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
